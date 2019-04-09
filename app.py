be279d9c70f39e5dde91e6474193a31c8063e967
yangf96 <fyang96c@gmail.com>
2019-04-09 20:55:49 +0800

    Add simple authentication, modify schema to allow types

--- app.py
+++ app.py
@@ -1,21 +1,21 @@
 import sqlite3
 import json
-from flask import Flask, g, jsonify
+from flask import Flask, g, jsonify, make_response, render_template
+from flask_httpauth import HTTPBasicAuth 
 
 DATABASE = './personalfinance.db'
 
 app = Flask(__name__)
+auth = HTTPBasicAuth()
 
 def make_dicts(cursor, row):
-    """Dictionary row factory for query results
-    """
+    """Dictionary row factory for query results"""
     return dict((cursor.description[idx][0], value)
             for idx, value in enumerate(row))
 
 
 def get_db():
-    """Connect to sqlite db
-    """
+    """Connect to sqlite db"""
     db = getattr(g, '_database', None)
     if db is None:
         db = g._database = sqlite3.connect(DATABASE)
@@ -25,14 +25,47 @@ def get_db():
 
 @app.teardown_appcontext
 def close_connection(exception):
-    """Close db connection
-    """
+    """Close db connection"""
     db = getattr(g, '_database', None)
     if db is not None:
         db.close()
 
 
-@app.route('/accounts')
+# User Authentication
+@auth.get_password
+def get_password(username):
+    cur = get_db().cursor()
+    cur.execute("SELECT password FROM users WHERE name=?", (username,))
+    password = cur.fetchone()['password']
+    return password
+ 
+
+@auth.error_handler
+def unauthorized():
+    return make_response(jsonify({'error': 'Unauthorized access'}), 401)
+
+
+# Render Templates
+@app.route('/')
+@auth.login_required
+def home():
+    cur = get_db().cursor()
+    cur.execute("SELECT name FROM users")
+    user = cur.fetchone()['name']
+    return render_template('index.html', data=user)
+    
+
+# RESTful API
+@app.route('/users', methods=['GET'])
+@auth.login_required
+def get_users():
+    cur = get_db().cursor()
+    cur.execute("SELECT * FROM users")
+    rows = cur.fetchall()
+    return jsonify(rows)
+
+
+@app.route('/accounts', methods=['GET'])
 def get_accounts():
     cur = get_db().cursor()
     cur.execute("SELECT * FROM accounts")
@@ -40,11 +73,13 @@ def get_accounts():
     return jsonify(rows) 
 
 
-@app.route('/transactions')
-def get_txns():
+@app.route('/transactions', methods=['GET'])
+def get_transactions():
     cur = get_db().cursor()
     cur.execute("SELECT * FROM transactions")
     rows = cur.fetchall()
     return jsonify(rows)
 
 
+if __name__ == '__main__':
+    app.run(debug=True)
--- initdb.py
+++ initdb.py
@@ -12,31 +12,41 @@ conn = sqlite3.connect(db_file)
 cur = conn.cursor()
 
 # Initialize database
+sql_create_users_table = """CREATE TABLE users (
+                             id INTEGER PRIMARY KEY AUTOINCREMENT,
+                             name TEXT NOT NULL UNIQUE,
+                             password TEXT NOT NULL,
+                             email TEXT,
+                             role TEXT CHECK( role IN ('admin','user') ) NOT NULL DEFAULT 'user'
+                         );"""
+
 sql_create_accounts_table = """CREATE TABLE accounts (
-                                id INTEGER PRIMARY KEY, 
+                                id INTEGER PRIMARY KEY AUTOINCREMENT, 
                                 name TEXT NOT NULL,
                                 balance REAL 
                             );"""
 
 sql_create_txn_table = """CREATE TABLE transactions (
-                            id INTEGER PRIMARY KEY,
-                            type INTEGER NOT NULL,
+                            id INTEGER PRIMARY KEY AUTOINCREMENT,
+                            type TEXT CHECK( type IN ( 'expense','income' ) ) NOT NULL,
                             title TEXT NOT NULL,
                             description TEXT,
                             account_id INTEGER NOT NULL,
                             FOREIGN KEY (account_id) REFERENCES accounts(id)
                        );"""
 
-cur.execute(sql_create_accounts_table)
+cur.execute(sql_create_users_table)
+cur.execute("INSERT INTO users (name,password,email,role) VALUES('admin', 'admin', 'admin@email.com', 'admin');")
 conn.commit()
-cur.execute("INSERT INTO accounts VALUES(1, 'Cash', '322.50');")
-cur.execute("INSERT INTO accounts VALUES(2, 'DBS Savings', '1250.86');")
+
+cur.execute(sql_create_accounts_table)
+cur.execute("INSERT INTO accounts (name,balance) VALUES('Cash', '322.50');")
+cur.execute("INSERT INTO accounts (name,balance) VALUES('DBS Savings', '1250.86');")
 conn.commit()
 
 cur.execute(sql_create_txn_table)
-conn.commit()
-cur.execute("INSERT INTO transactions VALUES('1', '0', 'Food', 'Lunch', '1');")
-cur.execute("INSERT INTO transactions VALUES('2', '1', 'Salary', 'March 19', '2');")
+cur.execute("INSERT INTO transactions (type,title,description,account_id) VALUES('expense', 'Food', 'Lunch', 1);")
+cur.execute("INSERT INTO transactions (type,title,description,account_id) VALUES('income', 'Salary', 'March 19', 2);")
 conn.commit()
 
 conn.close()
