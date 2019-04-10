c978a47b80a86c7fcaa7bdaba991b011dd131a65
yangf96 <fyang96c@gmail.com>
2019-04-10 21:37:06 +0800

    Replace raw SQL queries with SQLAlchemy

---
+++ app/models.py
@@ -0,0 +1,43 @@
+from datetime import datetime
+from werkzeug.security import generate_password_hash, check_password_hash
+from app import db
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    username = db.Column(db.String(30), index=True, unique=True)
+    password_hash = db.Column(db.String(20))
+    email = db.Column(db.String(20))
+    role = db.Column(db.String(8), default='user')
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+    
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+    def __repr__(self):
+        return '<User {}>'.format(self.username)
+
+
+class Account(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(30), nullable=False)
+    balance = db.Column(db.Float, default=0.0)
+    transactions = db.relationship('Transaction', backref='account', lazy=True)
+
+    def __repr__(self):
+        return '<Account {}, {}>'.format(self.name, self.balance)
+
+
+class Transaction(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    type = db.Column(db.String(10), nullable=False)
+    title = db.Column(db.String(30), nullable=False)
+    description = db.Column(db.String(50))
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    account_id = db.Column(db.Integer, db.ForeignKey('account.id'), nullable=False)
+ 
+    def __repr__(self):
+        return '<Transaction {}, {}>'.format(self.type, self.title)
+
+
