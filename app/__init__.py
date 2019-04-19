10f0bd2f3ce78ef1f7137e33e97e75ff7b20ec8a
yangf96 <fyang96c@gmail.com>
2019-04-19 21:29:27 +0800

    Add login forms and index views

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,16 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_login import LoginManager
 from config import Config
 
+# Init flask app
 app = Flask(__name__)
 app.config.from_object(Config)
+
+# Configure middleware
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+login_manager = LoginManager(app)
 
-from app import models
+from app import routes, models
