010ad9d8f28bb2b9a0ea975a6eb23e5635eca673
yangf96 <fyang96c@gmail.com>
2019-09-27 22:32:27 +0800

    Create user profile page

--- app/routes.py
+++ app/routes.py
@@ -59,6 +59,7 @@ def register():
 @login_required
 def user(username):
     user = User.query.filter_by(username=username).first_or_404()
-    return render_template('user.html', user=user)
+    accounts = Account.query.filter_by(user_id=user.id).all()
+    return render_template('user.html', user=user, accounts=accounts)
 
 
